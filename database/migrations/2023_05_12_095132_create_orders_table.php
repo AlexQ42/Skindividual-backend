679c48260a36fb216afd29e3f069e780e03d280a
Sven Rill <sven.rill@hof-university.de>
2023-06-21 08:49:57 +0200

    changed: migration table orders

--- database/migrations/2023_05_12_095132_create_orders_table.php
+++ database/migrations/2023_05_12_095132_create_orders_table.php
@@ -11,7 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('orders', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
         });
